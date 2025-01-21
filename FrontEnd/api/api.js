29623143673d4d721de7d36e48bfef4d8edd916b
Lorenzo ORFAO <lorenzo.orfao@decathlon.com>
2025-01-21 10:53:59 +0100

    Edit - Rename the function for the API calls

--- FrontEnd/api/api.js
+++ FrontEnd/api/api.js
@@ -1,6 +1,6 @@
 const URL_API = "http://localhost:5678/api/works";
 
-export function fetchData(callback) {
+export function getProjectsData(callback) {
     fetch(URL_API)
         .then(response => response.json())
         .then(data => {
