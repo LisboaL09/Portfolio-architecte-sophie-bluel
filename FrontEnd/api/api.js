19ac86f4503d70a22220b43851aa9179d7e0126f
Lorenzo ORFAO <lorenzo.orfao@decathlon.com>
2025-01-21 13:46:17 +0100

    Edit - api.js file and the script in index.html file are more clean

--- FrontEnd/api/api.js
+++ FrontEnd/api/api.js
@@ -1,10 +1,11 @@
-const URL_API = "http://localhost:5678/api/works";
-
-export function getProjectsData(callback) {
-    fetch(URL_API)
-        .then(response => response.json())
-        .then(data => {
-            callback(data);
-        })
-        .catch(error => console.error('Erreur dans la récupération des données:', error));
+// À optimiser pour la rendre async, retirer un des then, gérer le côté asynchrone et renvoyer juste les datas, la gestion de données se fait
+// sur l'index.html, sans la fonction de callback mais avec des then and catch
+export async function getProjectsData() {
+    try {
+        const response = await fetch("http://localhost:5678/api/works");
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Erreur dans la récupération des données:', error);
+    }
 }
