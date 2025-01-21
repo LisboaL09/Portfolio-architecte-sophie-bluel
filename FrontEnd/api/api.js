91db0211e94cc0a483e5eb6afe3545b4fef2b14c
Lorenzo ORFAO <lorenzo.orfao@decathlon.com>
2025-01-21 13:54:26 +0100

    Delete useless comments

--- FrontEnd/api/api.js
+++ FrontEnd/api/api.js
@@ -1,5 +1,3 @@
-// À optimiser pour la rendre async, retirer un des then, gérer le côté asynchrone et renvoyer juste les datas, la gestion de données se fait
-// sur l'index.html, sans la fonction de callback mais avec des then and catch
 export async function getProjectsData() {
     try {
         const response = await fetch("http://localhost:5678/api/works");
