faf0e1083b7ba902921eb08ba98e5f389f9c3baf
Lorenzo ORFAO <lorenzo.orfao@decathlon.com>
2025-01-23 09:44:56 +0100

    Fix - 2 separates function to improve the readability

--- FrontEnd/api/api.js
+++ FrontEnd/api/api.js
@@ -1,13 +1,19 @@
-export async function getSwaggerData() {
+export async function getCategoriesData() {
     try {
         const response_categories = await fetch("http://localhost:5678/api/categories");
-        const response_works = await fetch("http://localhost:5678/api/works");
-
         const data_categories = await response_categories.json();
-        const data_works = await response_works.json();
-
-        return { data_categories, data_works };
+        return data_categories;
     } catch (error) {
-        console.error('Erreur dans la récupération des données:', error);
+        console.error('Erreur dans la récupération des données de catégories :', error);
+    }
+}
+
+export async function getWorksData () {
+    try {
+        const response_works = await fetch("http://localhost:5678/api/works");
+        const data_works = await response_works.json();
+        return data_works;
+    } catch (error) {
+        console.error('Erreur dans la récupération des données de projets :', error);
     }
 }
