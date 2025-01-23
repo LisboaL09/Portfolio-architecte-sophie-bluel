ceed1cbfd794bcd6f5724170eb22227481e7363c
Lorenzo ORFAO <lorenzo.orfao@decathlon.com>
2025-01-23 10:06:50 +0100

    Fix - it work again

--- FrontEnd/js/activeButton.js
+++ FrontEnd/js/activeButton.js
@@ -1,4 +1,7 @@
 export function setActiveButton(activeButton) {
+
+    const filter = document.querySelector('.filter');
+
     const buttons = filter.querySelectorAll('button');
     buttons.forEach(button => {
         button.classList.remove('active');
