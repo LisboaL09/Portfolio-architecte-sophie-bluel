29f3f8af36ca26fffba9cf4594ffcaa0bb6c3a41
Lorenzo ORFAO <lorenzo.orfao@decathlon.com>
2025-01-28 15:57:27 +0100

    Edit - the form html and js file

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,32 +1,40 @@
+
 console.log('login.js')
-document.getElementById('connexion').addEventListener('click', async (event) => {
 
-    sessionStorage.getItem('userToken');
-    console.log('connexion');
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+function Login(){
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
 
-    try {
-        const response = await fetch('http://localhost:5678/api/users/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ email, password })
-        });
+    document.getElementById('login-button').addEventListener('click', async (event) => {
+        event.preventDefault();
+        sessionStorage.getItem('userToken');
 
-        if (response.ok) {
-            const data = await response.json();
-            const userToken = data.token;
-            sessionStorage.setItem('userEmail', email);
-            sessionStorage.setItem('userToken', userToken);
-            window.location.replace('/Portfolio-architecte-sophie-bluel/FrontEnd/');
-        } else {
-            const errorData = await response.json();
-            alert(errorData.message || 'Erreur de connexion');
+        try {
+            const response = await fetch('http://localhost:5678/api/users/login' ,{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    accept:'application/json'
+                },
+                body: JSON.stringify({ email: email.value , password: password.value })
+            });
+    
+            if (response.ok) {
+                const data = await response.json();
+                const userToken = data.token;
+                sessionStorage.setItem('userEmail', email);
+                sessionStorage.setItem('userToken', userToken);
+                window.location.replace('/Portfolio-architecte-sophie-bluel/FrontEnd/');
+            } else {
+                const errorData = await response.json();
+                alert(errorData.message || 'Erreur de connexion');
+            }
+        } catch (error) {
+            console.error('Erreur :', error);
+            alert('Erreur de connexion');
         }
-    } catch (error) {
-        console.error('Erreur:', error);
-        alert('Erreur de connexion');
-    }
-});
+    });
+
+}
+
+Login();
